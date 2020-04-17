4bdf70b93d0df8c9d5bdd4fe85fbce0f0b9b1a88
fanchessfan <54867984+fanchessfan@users.noreply.github.com>
2020-04-18 07:52:08 +0800

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,2 @@
+curl -X POST -u "apikey:NIHcwrQ2h3U9lIRi2VbeBo8gDO-gAUxj7q53-oUhLhm3" --header "Content-Type: audio/flac" --data-binary @Gov_Newsom_audio.flac "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/6f3c35e0-ebb8-4e13-959c-8ef460448f14/v1/recognize" >> ibm_text.txt
+
